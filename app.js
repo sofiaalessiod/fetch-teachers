3ba606666471ff2d944b5b97f385e16dfbda5982
sofiaalessiod <eaa23sead@students.eaaa.dk>
2024-05-27 12:31:42 +0200

    yea

--- app.js
+++ app.js
@@ -3,6 +3,27 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const teachers = await getTeachers();
+  console.log (teachers);
+  displayTeachers(teachers);
+}
+
+async function getTeachers() {
+  const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json"); //Fetch data from the API
+  const data = await response.json();
+  return data;
+}
+
+getTeachers(); //need this - calls the function so it shows up on the browser
+
+function displayTeachers(teachers) {
+  console.log(teachers);
+
+  const teachersList = document.querySelector("#teachers-list");
+  
+  for (const teacher of teachers) {
+    teachersList.insertAdjacentHTML("beforeend", `<li>${teacher.name}</li>`);
+  }
 }
